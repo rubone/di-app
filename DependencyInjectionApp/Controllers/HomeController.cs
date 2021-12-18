5b0efc0791367f137ad5e86f7ea40474cce263f9
rubone <rubone02@gmail.com>
2021-12-18 11:50:59 -0600

    :construction: Using operations interfaces in home controller

--- DependencyInjectionApp/Controllers/HomeController.cs
+++ DependencyInjectionApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using DependencyInjectionApp.Models;
+﻿using DependencyInjectionApp.Interfaces;
+using DependencyInjectionApp.Models;
+using DependencyInjectionApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,13 +15,41 @@ namespace DependencyInjectionApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly OperationService _operationService;
+        
+        private readonly IOperationTransient _operationTransient;
+
+        private readonly IOperationScoped _operationScoped;
+
+        private readonly IOperationSingleton _operationSingleton;
+
+        private readonly IOperationSingletonInstance _operationSingletonInstance;
+
+
+        public HomeController(ILogger<HomeController> logger,
+            OperationService operationService,
+            IOperationTransient operationTransient,
+            IOperationScoped operationScoped,
+            IOperationSingleton operationSingleton,
+            IOperationSingletonInstance operationSingletonInstance)
         {
             _logger = logger;
+            _operationService = operationService;
+            _operationTransient = operationTransient;
+            _operationScoped = operationScoped;
+            _operationSingleton = operationSingleton;
+            _operationSingletonInstance = operationSingletonInstance;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Transient = _operationTransient;
+            ViewBag.Scoped = _operationScoped;
+            ViewBag.Singleton = _operationSingleton;
+            ViewBag.SingletonInstance = _operationSingletonInstance;
+
+            ViewBag.Service = _operationService;
+
             return View();
         }
 
