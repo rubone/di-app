66e6d9de3f66d0c22c5caf8b42be547e8584c528
rubone <rubone02@gmail.com>
2021-12-18 11:48:28 -0600

    :construction: Adding operation services

---
+++ DependencyInjectionApp/Services/OperationService.cs
@@ -0,0 +1,23 @@
+﻿using DependencyInjectionApp.Interfaces;
+
+namespace DependencyInjectionApp.Services
+{
+    public class OperationService
+    {
+        public IOperationTransient OperationTransient { get; }
+
+        public IOperationScoped OperationScoped { get;  }
+
+        public IOperationSingleton OperationSingleton { get;  }
+
+        public IOperationSingletonInstance OperationSingletonInstance { get;  }
+
+        public OperationService(IOperationTransient operationTransient, IOperationScoped operationScoped, IOperationSingleton operationSingleton, IOperationSingletonInstance operationSingletonInstance)
+        {
+            OperationTransient = operationTransient;
+            OperationScoped = operationScoped;
+            OperationSingleton = operationSingleton;
+            OperationSingletonInstance = operationSingletonInstance;
+        }
+    }
+}
