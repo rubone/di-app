5f356b2e6d8882b808c546ee9ab893d491b36169
rubone <rubone02@gmail.com>
2021-12-18 11:49:49 -0600

    :construction: Inject services

--- DependencyInjectionApp/Startup.cs
+++ DependencyInjectionApp/Startup.cs
@@ -1,3 +1,6 @@
+using DependencyInjectionApp.Classes;
+using DependencyInjectionApp.Interfaces;
+using DependencyInjectionApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +27,11 @@ namespace DependencyInjectionApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IOperationTransient, Operation>();
+            services.AddScoped<IOperationScoped, Operation>();
+            services.AddSingleton<IOperationSingleton, Operation>();
+            services.AddSingleton<IOperationSingletonInstance>(new Operation(Guid.Empty));
+            services.AddTransient<OperationService, OperationService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
