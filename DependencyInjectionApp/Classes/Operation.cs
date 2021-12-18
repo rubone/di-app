a4800fe5cf12b400b848270bdd6708f0f7a63083
rubone <rubone02@gmail.com>
2021-12-18 11:47:53 -0600

    :construction: Adding Opertation class for implement interfaces

---
+++ DependencyInjectionApp/Classes/Operation.cs
@@ -0,0 +1,19 @@
+﻿using DependencyInjectionApp.Interfaces;
+using System;
+
+namespace DependencyInjectionApp.Classes
+{
+    public class Operation : IOperationTransient, IOperationScoped, IOperationSingleton, IOperationSingletonInstance
+    {
+        Guid _guid;
+
+        public Operation() : this(Guid.NewGuid()) { }
+
+        public Operation(Guid guid)
+        {
+            _guid = guid;
+        }
+        
+        public Guid OperationId => _guid;
+    }
+}
